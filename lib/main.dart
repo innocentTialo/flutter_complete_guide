559146d32929f32677903050252d4002d817fbeb
InnocentTIALO <tialoinnocent@gmail.com>
2020-03-25 11:31:35 +0100

    make introduction to StatefulWidget and how it works!

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,29 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  void answerQuestion() {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = (_questionIndex + 1) % 2;
+    });
     print("Answer chosen!");
+    print("question index is now $_questionIndex");
   }
 
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      "What is your favorite color?",
+      "What is your favorite animals?"
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -16,21 +32,20 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text('The question!'),
+            Text(
+              questions.elementAt(_questionIndex),
+            ),
             RaisedButton(
               child: Text('Answer 1'),
-              onPressed: answerQuestion,
+              onPressed: _answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 2'),
-              onPressed: () => print("Answer question 2"),
+              onPressed: _answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 3'),
-              onPressed: () {
-                //...
-                print("Answer question 3!");
-              },
+              onPressed: _answerQuestion,
             ),
           ],
         ),
