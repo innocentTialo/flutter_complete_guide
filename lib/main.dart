46fa120a9bf10f7c4e53ed7da3709c53da6a60ef
InnocentTIALO <tialoinnocent@gmail.com>
2020-03-28 17:09:44 +0100

    reinitialisation of app

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,26 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_complete_guide/quiz.dart';
-import 'package:flutter_complete_guide/result.dart';
-
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  final _questions = const [
-    {
-      'questionText': "What is your favorite color?",
-      'answers': ['Black', 'Green', 'Blue', 'Yellow'],
-    },
-    {
-      'questionText': "What is your favorite animals?",
-      'answers': ['Cat', 'Dog', 'Lion', 'Tigger'],
-    },
-    {
-      'questionText': "Who is your favorite instructor?",
-      'answers': ['Max', 'Mateo', 'Tchoupe', 'Tadmon'],
-    },
-  ];
-  Map<String, String> _results = Map();
-  var _questionIndex = 0;
-  void _answerQuestion(String answer) {
-    setState(() {
-      _results[_questions[_questionIndex]['questionText']] = answer;
-      _questionIndex = (_questionIndex + 1);
-    });
-  }
-
-  void _restartQuiz() {
-    setState(() {
-      print(_results);
-      _questionIndex = 0;
-      _results = Map();
-    });
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My First App'),
-        ),
-        body: _questionIndex < _questions.length
-            ? Quiz(
-                questions: _questions,
-                answerQuestion: _answerQuestion,
-                questionIndex: _questionIndex)
-            : Result(_restartQuiz, _results),
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Center(
+        child: Text('Widget Playground!'),
       ),
     );
   }
