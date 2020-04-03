18295af0b6f10672ae7cd7a072ed166371a16b8f
InnocentTIALO <tialoinnocent@gmail.com>
2020-04-03 14:08:15 +0100

    Initialisation of the third project

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_complete_guide/quiz.dart';
-import 'package:flutter_complete_guide/result.dart';
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
@@ -13,37 +10,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final _questions = const [
-    {
-      'questionText': "What is your favorite color?",
-      'answers': ['Black', 'Green', 'Blue', 'Yellow'],
-    },
-    {
-      'questionText': "What is your favorite animals?",
-      'answers': ['Cat', 'Dog', 'Lion', 'Tigger'],
-    },
-    {
-      'questionText': "Who is your favorite instructor?",
-      'answers': ['Max', 'Mateo', 'Tchoupe', 'Tadmon'],
-    },
-  ];
-  Map<String, String> _results = Map();
-  var _questionIndex = 0;
-  void _answerQuestion(String answer) {
-    setState(() {
-      _results[_questions[_questionIndex]['questionText']] = answer;
-      _questionIndex = (_questionIndex + 1);
-    });
-  }
-
-  void _restartQuiz() {
-    setState(() {
-      print(_results);
-      _questionIndex = 0;
-      _results = Map();
-    });
-  }
-
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -51,12 +18,7 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: _questionIndex < _questions.length
-            ? Quiz(
-                questions: _questions,
-                answerQuestion: _answerQuestion,
-                questionIndex: _questionIndex)
-            : Result(_restartQuiz, _results),
+        body: Container()
       ),
     );
   }
